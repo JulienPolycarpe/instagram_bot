85749b6912f28940d50387038dd97f04a7884728
julien <jujupoly@orange.fr>
2020-10-01 07:55:02 +0200

    bot working: follow, unfollow + info

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 from InstagramAPI import InstagramAPI
-from time import time
+from time import time, sleep
+import logging
+import threading
 
 class Bot(object):
 	def __init__(self, username, password, img_folder, caption_file, posts_per_day, follows_per_day, keyword):
+		format = "%(asctime)s: %(message)s"
+		logging.basicConfig(format=format, level=logging.INFO, datefmt="[%D] %H:%M:%S")
 		self.username = username
 		self.password = password
 		self.img_folder = img_folder
@@ -11,7 +15,6 @@ class Bot(object):
 		self.time_between_posts = 24 * 3600 / posts_per_day
 		self.follows_per_day = follows_per_day
 		self.time_between_follows = 24 * 3600 / follows_per_day
-		self.start_time = time()
 		self.keyword = keyword
 		self.bot = self.initBot()
 		self.followers = []
@@ -20,6 +23,18 @@ class Bot(object):
 		self.updateFollowers()
 		self.updateFollowings()
 		self.updateToFollow()
+
+	@property
+	def followers_nb(self):
+		return len(self.followers)
+
+	@property
+	def followings_nb(self):
+		return len(self.followings)
+
+	@property
+	def to_follow_nb(self):
+		return len(self.to_follow)
 
 	def initBot(self):
 		followers = []
@@ -32,9 +47,11 @@ class Bot(object):
 
 	def updateFollowers(self):
 		self.followers = [str(user['pk']) for user in self.bot.getTotalSelfFollowers()]
+		logging.info(f"Loaded {len(self.followers)} followers")
 
 	def updateFollowings(self):
 		self.followings = [str(user['pk']) for user in self.bot.getTotalSelfFollowings()]
+		logging.info(f"Loaded {len(self.followings)} followings")
 
 	def updateToFollow(self):
 		if(self.bot.tagFeed(self.keyword)):
@@ -45,12 +62,58 @@ class Bot(object):
 						pk = str(liker["pk"])
 						if (pk not in self.to_follow):
 							self.to_follow.append(pk)
+			logging.info(f"Loaded {len(self.to_follow)} persons to follow")
+		else:
+			logging.info(f"Failed to get persons to follow")
+	
+	def follow(self):
+		while True:
+			sleep(self.time_between_follows)
+			if (self.to_follow_nb == 0):
+				self.updateToFollow()
+			if (self.to_follow_nb == 0):
+				logging.info("Nobody to follow")
+			else:
+				next_follow = self.to_follow.pop(0)
+				self.bot.follow(next_follow)
+				logging.info(f"Successfully followed {next_follow}")				
 
+	def unfollow(self):
+		while True:
+			sleep(self.time_between_follows)
+			if (self.followings_nb == 0):
+				self.updateFollowings()
+			if (self.followings_nb == 0):
+				logging.info("Nobody to unfollow")
+			else:
+				next_unfollow = self.followings.pop(0)
+				self.bot.unfollow(next_unfollow)
+				logging.info(f"Successfully unfollowed {next_unfollow}")
+	
+	def toString(self):
+		return (f"{self.username} currently have "
+		f"{self.followers_nb} followers, {self.followings_nb} followings, and "
+		f"{self.to_follow_nb} persons to follow next")
+
+	def info(self):
+		while True:
+			sleep(600)
+			logging.info(self.toString())
+
+	def startBotting(self):
+		logging.info("Starting the bot:\n" + self.toString())
+		follow_thread = threading.Thread(target=self.follow).start()
+		unfollow_thread = threading.Thread(target=self.unfollow).start()
+		info_thread = threading.Thread(target=self.info).start()
+
+			
+
+		
 test = Bot("streetartforeveryone",
 			"THEBIBILLOU033//",
 			"images",
 			"caption.txt",
 			4,
 			150,
-			"streertart")
-print(len(test.followers), len(test.followings), len(test.to_follow))
+			"streetart")
+test.startBotting()
---
+++ test_thread.py
@@ -0,0 +1,27 @@
+import logging
+import threading
+import time
+
+def thread_function(name):
+	logging.info("Thread %s: starting", name)
+	time.sleep(5)
+	logging.info("Thread %s: finishing", name)
+
+def thread_function2(name):
+	logging.info("Thread %s: starting", name)
+	time.sleep(2)
+	logging.info("Thread %s: finishing", name)
+
+if __name__ == "__main__":
+	format = "%(asctime)s: %(message)s"
+	logging.basicConfig(format=format, level=logging.INFO, datefmt="[%D] %H:%M:%S")
+
+	logging.info("Main    : before creating thread")
+	x = threading.Thread(target=thread_function, args=("x",))
+	y = threading.Thread(target=thread_function2, args=("y",))
+	logging.info("Main    : before running thread")
+	x.start()
+	y.start()
+	logging.info("Main    : wait for the thread to finish")
+	# x.join()
+	logging.info("Main    : all done")
